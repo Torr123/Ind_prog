7fee57764d80f96bdb670baf8234324f5abd8b80
Highoc <door0172@gmail.com>
2017-09-30 01:00:25 +0300

    Review Онегина
    Хороший уровень - у тебя есть шансы закончить этот курс)

--- Evgeniy_Onegin/Evgeniy_Onegin.c
+++ Evgeniy_Onegin/Evgeniy_Onegin.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include"evg.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "evg.h"
 
 //#define OUT_FILE1 "Evgeniy_Onegin_BETTER.txt"
 //#define OUT_FILE2 "Evgeniy_Onegin_DA_BEST.txt"
-
 
 int main(int argc, char* argv[])
 {
@@ -13,12 +12,15 @@ int main(int argc, char* argv[])
 	char* Out_File_Rev = "Evgeniy_Onegin_DA_REVERCE.txt";
 	char* In_File = "Evgeniy.txt";
 
-	int r = 0, abc = 0;
-
+	int r = 0, abc = 0; 		//Это то чего я хотел - более интересная реализация этой идеи будет потом
+								//А так каждая функция должна выполнять одну логическую функцию
+								//Можно выделить обработку аргументов коммадной строки в отдельную функцию, а эти значения сделать глобальными
+								//Но это на твое усмотрение	
+	
 	for(int i = 0; i < argc; i++)
 	{
 		if(strcmp(argv[i], "-r") == 0)
-			r++;
+			r++; 
 		if(strcmp(argv[i], "-abc") == 0)
 			abc++;
 		if(strcmp(argv[i], "-i") == 0)
@@ -37,32 +39,33 @@ int main(int argc, char* argv[])
 
 	if(abc > 0 && r == 0)
 	{
-		qsort(Lines,NumOfLines, sizeof(char **), Cmp);
+		qsort(Lines,NumOfLines, sizeof(char **), Cmp); //А почему char **? Мы же сортируем char* - в данном случае это работает, тк sizeof(char **) = sizeof(char *) = разрядности системы  
 
-		FPrintLines(Out_File,Lines,NumOfLines);
+		FPrintLines(Out_File, Lines, NumOfLines);
 
 		return 0;
 	}
+	
 	if(r > 0 && abc == 0)
 	{
 		Rev_Lines(Lines, NumOfLines);
-		qsort(Lines,NumOfLines, sizeof(char **), Cmp);
+		qsort(Lines, NumOfLines, sizeof(char **), Cmp);
 		Rev_Lines(Lines, NumOfLines);
 
 		FPrintLines(Out_File_Rev,Lines,NumOfLines);
 
 		return 0;
 	}
+	//а эта вся часть кода зачем?
+	qsort(Lines, NumOfLines, sizeof(char **), Cmp);
 
-	qsort(Lines,NumOfLines, sizeof(char **), Cmp);
-
-	FPrintLines(Out_File,Lines,NumOfLines);
+	FPrintLines(Out_File, Lines, NumOfLines);
 
 	Rev_Lines(Lines, NumOfLines);
 	qsort(Lines,NumOfLines, sizeof(char **), Cmp);
 	Rev_Lines(Lines, NumOfLines);
 
 	FPrintLines(Out_File_Rev,Lines,NumOfLines);
-
+	//До сюда
 	return 0;
 }
