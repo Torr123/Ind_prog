d1c7bc7fbf957c3c694ea1856183415c40be51bf
Highoc <door0172@gmail.com>
2017-09-30 00:27:29 +0300

    Ревью кода
    Здесь подправить на 5 минут - как подправишь - напиши, и я зачту в таблицу.

--- Quadro/quadratic_equations.cpp
+++ Quadro/quadratic_equations.cpp
@@ -1,16 +1,23 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+//Я комментами буду писать, что не так и почему
+//Так эта прога написана хорошо, нужно подправить пару моментов
+#include <stdio.h>		//А ты уважаешь космос? Пробелов мало не бывает (здесь по ним все норм - на будущее)
+#include <stdlib.h>		//Лучше использовать не табуляцию, а пробелы (т.к. в разных редакторах таб может быть = 2, 4 или 8 пробелам и все будет съезжать).
+#include <math.h>		//В настройках своей среды можно настроить автозамену табов на пробелы.
 
-#define SS_INF_ROOTS 9
+#define SS_INF_ROOTS 9  //Можно использовать си++ лайфхаки вроде констант времени компиляции (constexpr int SS_INF_ROOTS = 9) - они лучше тупой подстановки define
 
-int SolveSquare(double a, double b, double c,double* x1, double* x2)
+int SolveSquare(double a, double b, double c, double* x1, double* x2)
 {
-	if (a == 0)
-	{
+	//Нужно думать о возможных ошибках - их обработка одна из главных частей промышленной проги
+	//Если ту вызовешь функцию с аргументом в виде нулевого указателя *x1 дропнет прогу
+	//Проверяй их assert или if
+	if (a == 0) //double сравнивать с нулем опасно - у него есть погрешность. 
+	{			//лучше сравнивать abs(a) < E , где E - погрешность double; она есть в c++ библиотеки математики или выбери 1e-4
+				//Проверку с даблом ты используешь минимум 4 раза в этой проге - лучше тогда уж вынести ее в отдельную функцию
+				//Тк копипаст это плохо
 		if (b == 0)
 		{
-			return (c == 0)? SS_INF_ROOTS : 0;
+			return (c == 0)? SS_INF_ROOTS : 0; 		//Тернарный оператор вывода - похвально
 		}
 		else
 		{
@@ -21,7 +28,7 @@ int SolveSquare(double a, double b, double c,double* x1, double* x2)
 	else
 	{
 		double d;
-		d = b*b - (4*a*c);
+		d = b*b - (4*a*c); // Можно объединить инициализацию и определение double d = b*b - (4*a*c);
 		if (d == 0)
 		{ 
 			*x2 = -(b / (2*a));
@@ -38,6 +45,8 @@ int SolveSquare(double a, double b, double c,double* x1, double* x2)
 	}
 }
 
+//Хорошим тоном было бы если при заходе в библиотеку или твой файл, я сначало видел main, чтобы видеть самую главную часть твоей проги
+//Вынеси объявления функций наверх, потом main, потом их реализацию
 int main()
 { 
 	printf("# Enter a, b, c: ");
@@ -55,7 +64,7 @@ int main()
 		break;
 	case SS_INF_ROOTS: printf ("Any number");
 			break;
-	default: printf ("main(): ERROR: nRoots = %d\n",nRoots);
+	default: printf ("main(): ERROR: nRoots = %d\n", nRoots);
 	return 1;
 	}
 }
