2ec6f0e9ae44ec4dbc0ed4d74e0cd437f6ddd065
Highoc <door0172@gmail.com>
2017-09-30 01:21:40 +0300

    Review
    Немного поправок. Кстати давать длинные имена сишникам, ашникам, переменным  не плохо - видел бы ты их в java. Главное чтобы то, кто первый раз читает понял. И главное в меру.

--- Evgeniy_Onegin/lib/File.c
+++ Evgeniy_Onegin/lib/File.c
@@ -2,18 +2,21 @@
 #include<stdlib.h>
 #include"evg.h"
 
-char* ReadbinFile(const char* FileName)
-{
-        FILE* file = fopen(FileName, "rb");
-
+char* ReadbinFile(const char* FileName) //Опять нужно проверять входные указатели на нуль
+{                                       //Даже в функции компараторе в сишнике выше
+        FILE* file = fopen(FileName, "rb"); 
+                                        //Пропала проблема когда написал rb?
+                                        //fopen() может вернуть нулевой указатель если не смог открыть файл
+                                        //Нужно чекать (часто помогает при отладке)
         int count = 0;
 
-        fseek(file, 0, SEEK_END);
+        fseek(file, 0, SEEK_END);       //Можно вынести в отдельную функцию "Получить размер файла"
                 count = ftell(file) + 1;
         fseek(file, 0, SEEK_SET);
 
         void* String = calloc(count, 1);
-
+                                        //calloc() тоже - правильно что ты проверяешь
+                                        //К каждому calloc'y дожен быть свой free - в main'e его нет
         if(String == NULL)
         {
                 printf("Not enough memory!");
@@ -25,9 +28,9 @@ char* ReadbinFile(const char* FileName)
         return (char *)String;
 }
 
-void FPrintLines(const char* FileName, char** lines, int numl)
+void FPrintLines(const char* FileName, char** lines, int numl) //А если нулевые указатели?
 {
-        FILE* New = fopen(FileName, "w");
+        FILE* New = fopen(FileName, "w"); //А если файл занят?
 
         for(int i = 0; i < numl; i++)
                 fprintf(New, "%s\n", lines[i]);
