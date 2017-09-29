393cfbcf6d132a24b7097e759703819464aec857
Highoc <door0172@gmail.com>
2017-09-30 01:30:34 +0300

    Review
    В общем все хорошо - мне понравилось, этот код Деду будет не стыдно показать.

--- Evgeniy_Onegin/lib/evg.h
+++ Evgeniy_Onegin/lib/evg.h
@@ -1,3 +1,6 @@
+//Сюда также можно вынести стандартные библиотеки, которые используются в этих функциях, чтобы не писать их каждый раз
+//И какой hник правильный?) у тебя их два
+
 char** StringtoLines(char* filestr, int* linesnum);
 void FPrintLines(const char* FileName, char** lines, int numl);
 char* ReadbinFile(const char* FileName);
