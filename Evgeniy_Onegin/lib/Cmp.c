4654a234ac973a893dd64fbb80fa0de9cadde84f
Torr123 <ynov@inbox.ru>
2017-09-30 15:51:56 +0300

    sixth with other Cmd function commit

--- Evgeniy_Onegin/lib/Cmp.c
+++ Evgeniy_Onegin/lib/Cmp.c
@@ -1,7 +1,27 @@
 #include<string.h>
+#include<ctype.h>
 #include"evg.h"
 
 int Cmp(const void* l, const void* l1)
 {
-        return strcmp(*(const char **)l, *(const char **)l1);
+	const char* line = *(const char **)l;
+	const char* line1 = *(const char **)l1;
+
+	int count = 0, count1 = 0;// Количество знаков отличающихся от алфавита
+
+	int strl = strlen(line), strl1 = strlen(line1);
+
+	for(count = 0; count < strl; count++)
+	{
+		if(isalpha(line[count]))
+			{break;}
+	}
+
+	for(count1 = 0; count1 < strl1; count1++)
+	{
+		if(isalpha(line1[count1]))
+			{break;}
+	}
+
+        return strcmp(line + count, line1 + count1);
 }
