9723c76a9510c324eae03fb2e66ee6b662641657
Highoc <door0172@gmail.com>
2017-10-09 17:52:39 +0300

    Review

--- Stack/lib/Stack.c
+++ Stack/lib/Stack.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
-
+/* Почему вместо этого не заинклюдить st.h?
 #define START_STACK_SIZE 5
 
 #define ASSERT_OK(stack_pointer) if(StackOk(stack_pointer)) \
@@ -19,13 +19,14 @@ struct CStack
 	int MaxCount;
 	int count;
 };
-
+*/
 void Stack_Dump(struct CStack* st)
 {
 	FILE* Dump = fopen("Dump_Stack", "at");
-
+	//Dump = null?	
 	fprintf(Dump,"CStack ");
 
+	/*
 	if(st == NULL)
 	{
 		fprintf(Dump,"[\"NULL\"] (ERROR!!!)");
@@ -34,7 +35,10 @@ void Stack_Dump(struct CStack* st)
 	{
 		fprintf(Dump, "[ %p ] (ok)", st);
 	}
-
+	*/
+	
+	//а можно короче: fprintf(Dump, "[%p] (%s)", (st) ? st : NULL, (st) ? "ok" : "ERROR!!!");
+	
 	fprintf(Dump,"\n{\n\tMaxCount = %i;\n\tcount = %i;", st->MaxCount, st->count);
 
 	if(st->count > st->MaxCount || st->count < 0)
@@ -74,6 +78,7 @@ const char* StackOk(const struct CStack* st)
 
 void Stack_Make(struct CStack* st)
 {
+	//st = 0 ?
 	st->data = (Stack_Type*)calloc(START_STACK_SIZE, sizeof(Stack_Type));
 	st->count = 0;
 	st->MaxCount = START_STACK_SIZE;
@@ -89,7 +94,8 @@ void Stack_Push(struct CStack* st, Stack_Type val)
 	st->data[st->count++] = val;
 
 	if(st->count > (st->MaxCount * 4)/5){st->data = (Stack_Type *)realloc((st->data), sizeof(Stack_Type)*(st->MaxCount += st->MaxCount/3));}
-
+	//можно вынести в отдельные функции
+	
 	ASSERT_OK(st);
 }
 
@@ -100,7 +106,8 @@ Stack_Type Stack_Pop(struct CStack* st)
 	Stack_Type buf = st->data[--st->count];
 
 	if(st->count < (st->MaxCount)/3 && st->count > 0){ st->data = (Stack_Type *)realloc((st->data), sizeof(Stack_Type)*(st->MaxCount /= 2));}
-
+	//можно вынести в отдельные функции
+	
 	ASSERT_OK(st);
 
 	return buf;
@@ -109,7 +116,9 @@ Stack_Type Stack_Pop(struct CStack* st)
 
 void Stack_Dest(struct CStack* st)
 {
-	ASSERT_OK(st);
-
+	ASSERT_OK(st); 
 	free(st->data);
+	//Если ты вызовешь assert_OK здесь - то скорее всего он тебе не выдаст ошибок => ты можешь работать с зомби =>
+	//Как настоящий джентельмен ты можешь загаживать эти данные, обнулять указатели и тд..
+	//Тем более два вызова деструктора от одного объекта крашнет все, а ты это своей проверкой не ловишь
 }
