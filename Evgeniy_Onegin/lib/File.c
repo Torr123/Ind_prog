82b33849f955d5ae29affb5de0a618ec37153ee5
Torr123 <ynov@inbox.ru>
2017-10-09 13:53:44 +0000

    Corrected

--- Evgeniy_Onegin/lib/File.c
+++ Evgeniy_Onegin/lib/File.c
@@ -1,18 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<assert.h>
 #include"evg.h"
+
+int SizeOfFile(FILE* file)
+{
+        int count = 0;
+        fseek(file, 0, SEEK_END);       //Можно вынести в отдельную функцию "Получить размер файла"
+        count = ftell(file) + 1;
+        fseek(file, 0, SEEK_SET);
+        return count;
+}
 
 char* ReadbinFile(const char* FileName) //Опять нужно проверять входные указатели на нуль
 {                                       //Даже в функции компараторе в сишнике выше
+        assert(FileName != NULL);
+        
         FILE* file = fopen(FileName, "rb"); 
                                         //Пропала проблема когда написал rb?
                                         //fopen() может вернуть нулевой указатель если не смог открыть файл
                                         //Нужно чекать (часто помогает при отладке)
-        int count = 0;
-
-        fseek(file, 0, SEEK_END);       //Можно вынести в отдельную функцию "Получить размер файла"
-                count = ftell(file) + 1;
-        fseek(file, 0, SEEK_SET);
+        assert(file != NULL);
+        int count = SizeOfFile(file);
 
         void* String = calloc(count, 1);
                                         //calloc() тоже - правильно что ты проверяешь
@@ -31,7 +40,7 @@ char* ReadbinFile(const char* FileName) //Опять нужно проверят
 void FPrintLines(const char* FileName, char** lines, int numl) //А если нулевые указатели?
 {
         FILE* New = fopen(FileName, "w"); //А если файл занят?
-
+        assert(New != NULL);
         for(int i = 0; i < numl; i++)
                 fprintf(New, "%s\n", lines[i]);
 }
